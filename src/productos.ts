eb2152f8bb36179fac3118ca6463e7a9b6d9aaec
Hans <hans@welinux.cl>
2023-11-12 14:14:14 -0300

    Saca /public de imágenes

--- src/productos.ts
+++ src/productos.ts
@@ -3,21 +3,21 @@ import { Producto } from "./Producto";
 export const productos: Producto[] = [
   {
     id: 1,
-    foto: "../public/images/projects/project1.jpg",
+    foto: "/images/projects/project1.jpg",
     nombre: "Equipo Hospitalario",
     descripcion:
       "Equipo hospitalario con gran variedad con: mobiliario clínico, compresores, alarmas, bombas de vacio, flujometros, manifolds , y otros muchos productos mas.",
   },
   {
     id: 2,
-    foto: "../public/images/projects/project2.jpg",
+    foto: "/images/projects/project2.jpg",
     nombre: "Equipo Medico",
     descripcion:
       "Equipo medico de alta calidad contando con productos tales como: cilindros, reguladores, carros porta cilindros, mascarillas, cánulas y humidificadores.",
   },
   {
     id: 3,
-    foto: "../public/images/projects/project3.jpg",
+    foto: "/images/projects/project3.jpg",
     nombre: "Equipo de Seguridad",
     descripcion:
       "Equipos para la seguridad respiratorial y corporal con productos tales como: respiradores, mascarillas, overoles, guantes y protectores para guante.",
