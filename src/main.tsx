23ec3d339bba2225c1e9b7781d4641ce6cfc8433
Hans <hans@welinux.cl>
2023-11-26 22:54:01 -0300

    Ofusca campos

--- src/main.tsx
+++ src/main.tsx
@@ -14,3 +14,34 @@ ReactDOM.createRoot(document.getElementById("form-contacto")!).render(
     <FormContacto />
   </React.StrictMode>
 );
+
+const condor = "NTY5MzkyMjIwMjY=";
+const puma = "Y29udGFjdG9AY3ltbWVkaWNhbC5jbA==";
+const ttt = atob(condor);
+
+(function () {
+  const a = /(\d\d)(\d)(\d{4})(\d{4})/.exec(ttt);
+  if (!a) return;
+  const els = document.getElementsByClassName("telefono");
+  Array.from(els).forEach((el) => {
+    ReactDOM.createRoot(el).render(
+      <a href={`tel:${ttt}`}>
+        +{a[1]} {a[2]} {a[3]} {a[4]}
+      </a>
+    );
+  });
+})();
+
+(function () {
+  const m = atob(puma);
+  const els = document.getElementsByClassName("email-contacto");
+  Array.from(els).forEach((el) => {
+    ReactDOM.createRoot(el).render(<a href={`mailto:${m}`}>{m}</a>);
+  });
+})();
+
+ReactDOM.createRoot(document.getElementById("wtsp-chat")!).render(
+  <a href={`https://wa.me/${ttt}`}>
+    <img alt="Chat on WhatsApp" src="images/wtsp.png" width="180" />
+  </a>
+);
